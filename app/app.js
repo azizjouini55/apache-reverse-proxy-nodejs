084df47db63be4df8562b52de9d43e0be7ac6c2d
Aziz-J <aziz-j@ismael.inf.tu-dresden.de>
2023-03-27 14:56:31 +0200

    adding passport saml strategy

--- app/app.js
+++ app/app.js
@@ -1,7 +1,50 @@
-var http = require('http');
+const SamlStrategy = require('passport-saml').Strategy;
+const express = require('express')
+const app = express();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World');
-}).listen(3000, "0.0.0.0");
-console.log('Server running ');
+// set a server variable
+app.use(function(req, res, next) {
+  res.locals.serverName = 'My Server'
+  next()
+});
+
+// route to display the server variable
+app.get('/', function(req, res) {
+  res.send('Server Name: ' + res.locals.serverName)
+});
+
+app.post('/login/callback',
+  passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/login',
+  passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.listen(3000,"0.0.0.0" ,function() {
+    console.log('Server listening on port 3000')
+  });
+
+
+passport.use(new SamlStrategy(
+    {
+      path: '/login/callback',
+      entryPoint: 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
+      issuer: 'passport-saml',
+      cert: fs.readFileSync('./cert.pem', 'utf-8'),
+    },
+    function(profile, done) {
+      findByEmail(profile.email, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        return done(null, user);
+      });
+    })
+  );
